51e7e0509b270bdc9cf08233ca303a80682ce894
Ruslan Shashkov <ruslan.s@expertoption.com>
2019-11-09 10:16:10 +0200

    Update

--- src/constants/urls.ts
+++ src/constants/urls.ts
@@ -1,7 +1,7 @@
 export const API_URLS = {
-  AUTH_LOGIN: `${process.env.API_URL}/auth/login`,
-  AUTH_REGISTER: `${process.env.API_URL}/auth/register`,
-  AUTH_ME: `${process.env.API_URL}/auth/me`,
-  PROPERTY_LIST: `${process.env.API_URL}/property`,
-  PROPERTY_ITEM: `${process.env.API_URL}/property/:id`,
+  AUTH_LOGIN: `${process.env.REACT_APP_API_URL}/auth/login`,
+  AUTH_REGISTER: `${process.env.REACT_APP_API_URL}/auth/register`,
+  AUTH_ME: `${process.env.REACT_APP_API_URL}/auth/me`,
+  PROPERTY_LIST: `${process.env.REACT_APP_API_URL}/property`,
+  PROPERTY_ITEM: `${process.env.REACT_APP_API_URL}/property/:id`,
 };
