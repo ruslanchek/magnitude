ad670ecb66e2479c884e30572a92fc3c521ebc53
Ruslan Shashkov <ruslanchek@gmail.com>
2019-11-17 22:20:13 +0200

    Upd

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -4,15 +4,20 @@ import React from 'react';
 import { PageWrapper } from '../components/PageWrapper';
 import { SocketApi } from '../api/SocketApi';
 import { AuthApi } from '../api/AuthApi';
+import { ESocketAction } from '@ruslanchek/magnitude-shared';
 
 SocketApi.connect();
+
+SocketApi.on(ESocketAction.AuthMe, e => {
+  console.log(e);
+});
 
 SocketApi.onConnectionChanged(connected => {
   if (connected) {
     // AuthApi.authorize();
     // AuthApi.register('rshashkov+10@icloud.com', 'tukzara1');
-    // AuthApi.login('rshashkov+10@icloud.com', 'tukzara1');
-    AuthApi.me();
+    AuthApi.login('rshashkov+10@icloud.com', 'tukzara1');
+    // AuthApi.me();
   }
 });
 
