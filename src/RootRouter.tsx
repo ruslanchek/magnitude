7b76d164e26cf7f529b271f4153e2c0621da2611
Ruslan Shashkov <rshashkov@icloud.com>
2019-12-09 15:08:39 +0200

    Auth

--- src/RootRouter.tsx
+++ src/RootRouter.tsx
@@ -1,11 +1,11 @@
-import React from "react";
+import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import { PATHS } from './constants/paths';
 import { HomeScreen } from './components/screens/HomeScreen';
 import { NoMatchPage } from './components/screens/NoMatchPage';
 import { TestScreen } from './components/screens/TestPage';
-import { LoginScreen } from "./components/screens/LoginScreen";
-import { RegisterScreen } from "./components/screens/RegisterScreen";
+import { LoginScreen } from './components/screens/LoginScreen';
+import { RegisterScreen } from './components/screens/RegisterScreen';
 
 export const RootRouter: React.FC = () => {
   return (
