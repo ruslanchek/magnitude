1ac19b126ee4f12fd6509c2ade022829a31ee88b
Ruslan Shashkov <rshashkov@icloud.com>
2019-12-24 18:13:57 +0200

    .trim() for CssVariablesHooks

--- src/hooks/useCssVariableNumber.ts
+++ src/hooks/useCssVariableNumber.ts
@@ -2,7 +2,12 @@ import { useMemo } from 'react';
 
 export function useCssVariableNumber(variableName: string, deps: any[] = []): number {
   return useMemo(() => {
-    let value = parseInt(getComputedStyle(document.documentElement).getPropertyValue(variableName), 10);
+    let value = parseInt(
+      getComputedStyle(document.documentElement)
+        .getPropertyValue(variableName)
+        .trim(),
+      10,
+    );
 
     if (isNaN(value)) {
       value = 0;
--- src/hooks/useCssVariableString.ts
+++ src/hooks/useCssVariableString.ts
@@ -2,6 +2,8 @@ import { useMemo } from 'react';
 
 export function useCssVariableString(variableName: string, deps: any[] = []): string {
   return useMemo(() => {
-    return getComputedStyle(document.documentElement).getPropertyValue(variableName);
+    return getComputedStyle(document.documentElement)
+      .getPropertyValue(variableName)
+      .trim();
   }, deps);
 }
