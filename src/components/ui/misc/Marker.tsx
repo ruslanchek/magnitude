cd993286099717fb2f734f2dc2113beed71f9014
Ruslan Shashkov <rshashkov@icloud.com>
2019-12-24 14:48:59 +0200

    Upd

---
+++ src/components/ui/misc/Marker.tsx
@@ -0,0 +1,18 @@
+/** @jsx jsx */
+import { jsx, css } from '@emotion/core';
+import React from 'react';
+
+export const Marker: React.FC = () => {
+  return <i css={styles.root} />;
+};
+
+const styles = {
+  root: css`
+    border-radius: 100%;
+    background-color: rgb(var(--ERROR));
+    width: 6px;
+    height: 6px;
+    display: block;
+    box-shadow: 0 0 0 3px rgba(var(--ERROR), 0.15);
+  `,
+};
---
+++ src/components/ui/typographics/CategoryTitle.tsx
@@ -0,0 +1,17 @@
+/** @jsx jsx */
+import { jsx, css } from '@emotion/core';
+import React from 'react';
+
+export const CategoryTitle: React.FC = ({ children }) => {
+  return <h3 css={styles.root}>{children}</h3>;
+};
+
+const styles = {
+  root: css`
+    color: rgb(var(--TEXT_FADED));
+    font-size: var(--FONT_SIZE_SMALL);
+    text-transform: uppercase;
+    font-weight: 600;
+    letter-spacing: 0.5px;
+  `,
+};
